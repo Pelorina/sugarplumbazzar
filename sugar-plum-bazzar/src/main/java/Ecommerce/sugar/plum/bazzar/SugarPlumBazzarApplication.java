ae1cad6747f28c8e25a00432a09222b0c4ea8623
pelorina <joaquimpelumi@gmail.com>
2023-09-15 04:20:22 -0700

    added cross origin

--- sugar-plum-bazzar/src/main/java/Ecommerce/sugar/plum/bazzar/SugarPlumBazzarApplication.java
+++ sugar-plum-bazzar/src/main/java/Ecommerce/sugar/plum/bazzar/SugarPlumBazzarApplication.java
@@ -2,8 +2,26 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+ @SpringBootApplication
 public class SugarPlumBazzarApplication {
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+
+				registry
+						.addMapping("/**")
+						.allowedOrigins("*", "http://127.0.0.1", "http://example.org", "http://localhost", "http://localhost:5500")
+						.allowedHeaders("*")
+						.allowedMethods("POST", "GET", "PUT", "PATCH", "OPTIONS");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SugarPlumBazzarApplication.class, args);
